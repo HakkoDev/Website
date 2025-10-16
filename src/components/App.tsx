ef018c4033dfa312045afb47f2d07a3b7c5acdc3
Hakko <67168651+HakkoDev@users.noreply.github.com>
2025-10-15 19:01:13 -0600

    Everything in its right place.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function App() {
     const canvasRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef(null);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -53,12 +55,43 @@ export default function App() {
         return () => window.removeEventListener("resize", resize);
     }, []);
 
+    const toggleMusic = () => {
+        const audio = audioRef.current;
+        if (!audio) return;
+
+        if (isPlaying) {
+            audio.pause();
+            setIsPlaying(false);
+        } else {
+            audio.volume = 0.3;
+            audio.play().catch((err) => console.log("Autoplay blocked:", err));
+        setIsPlaying(true);
+        }
+    };
+
+    useEffect(() => {
+        const audio = audioRef.current;
+        if (audio) {
+            audio.volume = 0.3;
+            audio.play().then(() => {setIsPlaying(true)}).catch(() => {
+                setIsPlaying(false);
+            });
+        }
+    }, []);
+
     return (
         <div className="relative flex flex-col items-center justify-center h-screen text-white bg-black overflow-hidden">
             <canvas
                 ref={canvasRef}
                 className="absolute inset-0 w-full h-full opacity-40"
             />
+
+            <div className="absolute top-5 right-5 rounded-full hover:text-gray-300 transition cursor-pointer" onClick={toggleMusic}>
+                {isPlaying ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9 1H2v1H1v20h1v1h7v-1h1V2H9zM8 3v18H3V3zm14-1V1h-7v1h-1v20h1v1h7v-1h1V2zm-1 1v18h-5V3z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M21 11v-1h-1V9h-2V8h-2V7h-1V6h-2V5h-2V4h-1V3H8V2H6V1H3v1H2v20h1v1h3v-1h2v-1h2v-1h1v-1h2v-1h2v-1h1v-1h2v-1h2v-1h1v-1h1v-2zm-2 2h-2v1h-2v1h-1v1h-2v1h-2v1H9v1H7v1H5v1H4V3h1v1h2v1h2v1h1v1h2v1h2v1h1v1h2v1h2z"/></svg> }
+            </div>
+
+            <audio ref={audioRef} src="/everything_in_its_right_place.mp3" loop autoPlay></audio>
+
             <div className="z-10 text-center px-6 max-w-2xl">
                 <h1 className="text-4xl font-bold mb-6 tracking-widest">Hakko</h1>
 
